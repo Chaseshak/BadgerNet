ad813ded47389500cba7d8468bcdc103851f9454
Chase Schachenman <chaseshak@gmail.com>
2017-09-27 11:45:10 -0500

    Add unicorn config bash script

---
+++ .unicorn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This file is meant to be executed via systemd.
+source /usr/local/rvm/scripts/rvm
+source /etc/profile.d/rvm.sh
+export ruby_ver=$(rvm list default string)
+
+export CONFIGURED=yes
+export TIMEOUT=50
+export APP_ROOT=/home/rails/BadgerNet
+export RAILS_ENV="production"
+export GEM_HOME="/home/rails/BadgerNet/vendor/bundle"
+export GEM_PATH="/home/rails/BadgerNet/vendor/bundle:/usr/local/rvm/gems/${ruby_ver}:/usr/local/rvm/gems/${ruby_ver}@global"
+export PATH="/home/rails/rails_project/vendor/bundle/bin:/usr/local/rvm/gems/${ruby_ver}/bin:${PATH}"
+
+# Passwords
+export SECRET_KEY_BASE=7aad3b84148bc92b77be068f2dd3c5c8924e6565a7a6586aa0b2d3b4bf413538282d247bcbdce8e4ebe00bc9dca97c8778871ecfa459421324fba9a2f9217236
+
+# Execute the unicorn process
+/home/rails/BadgerNet/vendor/bundle/bin/unicorn \
+        -c /etc/unicorn.conf -E production --debug
