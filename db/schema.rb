e626bd92397a82c826e9460c185bde277e930809
George Black <georgemblack@users.noreply.github.com>
2017-11-08 11:23:50 -0600

    Create model for schedules

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101202402) do
+ActiveRecord::Schema.define(version: 20171108172250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "announcements", force: :cascade do |t|
     t.string "content"
-    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
@@ -43,6 +42,12 @@ ActiveRecord::Schema.define(version: 20171101202402) do
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.json "events"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/schedules.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :schedule do
+    events ""
+  end
+end
